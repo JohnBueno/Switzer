49dff1cc4ac4258e75bbcc1a047a627e7934426f
John Bueno <bueno@facultycreative.com>
2014-10-28 10:14:05 -0400

    Quick fix solution for full min col height

--- wp-content/themes/roots-master/template-about.php
+++ wp-content/themes/roots-master/template-about.php
@@ -6,7 +6,7 @@ Template Name: About Template
 <ul id="cbp-bislideshow" class="cbp-bislideshow">
 	<li><img src="<?php bloginfo('template_url'); ?>/assets/img/home-bg-1.jpg" alt="image01"/></li>
 </ul>
-<div class="col-md-4 col-md-offset-6">
+<div class="col-md-4 col-md-offset-6 single-col">
 	<?php while (have_posts()) : the_post(); ?>
   		<?php get_template_part('templates/page', 'header'); ?>
   		<?php get_template_part('templates/content', 'about'); ?>
