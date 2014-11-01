de696ce7a5dec81e29cefab5f225af9b34d231d3
John Bueno <bueno@facultycreative.com>
2014-10-31 21:06:32 -0400

    list about siblings

--- wp-content/themes/roots-master/templates/content-about.php
+++ wp-content/themes/roots-master/templates/content-about.php
@@ -1,1 +1,8 @@
 <?php the_content(); ?>
+
+<ul class="siblings">
+  <?php
+  $parent = $post->post_parent;
+  wp_list_pages("title_li=&child_of=$parent&show_date=modified
+  &date_format=$date_format"); ?>
+</ul>
