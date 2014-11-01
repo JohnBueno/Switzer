4cc45b7a78e366b9d71cd1bc5c87c310b581ba68
John Bueno <bueno@facultycreative.com>
2014-10-31 21:06:15 -0400

    Added breadcrumbs

--- wp-content/themes/roots-master/templates/page-header.php
+++ wp-content/themes/roots-master/templates/page-header.php
@@ -1,5 +1,18 @@
 <div class="page-header">
-  <h1>
+<div class="breadcrumbs">
+	<?php 
+		// if there is a parent, display the link
+		$parent_title = get_the_title( $post->post_parent );
+		if ( $parent_title != the_title( ' ', ' ', false ) ) {
+			echo '<a href="' . get_permalink( $post->post_parent ) . '" title="' . $parent_title . '">' . $parent_title . '</a> / ';
+		} 
+		// then go on to the current page link
+	?>
+	<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
+		<?php the_title(); ?>
+	</a> 
+</div>
+  <h3>
     <?php echo roots_title(); ?>
-  </h1>
+  </h3>
 </div>
