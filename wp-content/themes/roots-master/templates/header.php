c91adccbc7e52ff247dfbcc0546739579cf3be5b
John Bueno <bueno@facultycreative.com>
2014-11-15 12:54:33 -0500

    set up basic slide out nav

--- wp-content/themes/roots-master/templates/header.php
+++ wp-content/themes/roots-master/templates/header.php
@@ -29,3 +29,11 @@
         </div>
     </header>
 </div>
+
+<div class="side-nav">
+    <?php
+        if (has_nav_menu('primary_navigation')) :
+            wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'slide'));
+        endif;
+    ?>
+</div>
