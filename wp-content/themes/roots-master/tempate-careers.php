8173a54f5661d60688009e2b1284eaf04aaadc34
John Bueno <bueno@facultycreative.com>
2014-11-17 21:32:52 -0500

    Added career template

---
+++ wp-content/themes/roots-master/tempate-careers.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: Careers Template
+*/
+?>
+
+<ul id="cbp-bislideshow" class="cbp-bislideshow">
+    <li><img src="<?php bloginfo('template_url'); ?>/assets/img/home-bg-1.jpg" alt="image01"/></li>
+</ul>
+<div class="shadow"></div>
+<div class="col-md-5 col-md-offset-6 col-sm-offset-6 col-sm-6 single-col fixed-col page">
+    <div class="extra-pad">
+        <?php while (have_posts()) : the_post(); ?>
+            <?php get_template_part('templates/page', 'header'); ?>
+            <?php get_template_part('templates/content', 'page'); ?>
+        <?php endwhile; ?>
+    </div>
+</div>
