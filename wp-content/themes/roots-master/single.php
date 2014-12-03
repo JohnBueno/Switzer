2a4e7b8d9716b210caf87c9ef711dc459af22644
John Bueno <bueno@facultycreative.com>
2014-12-02 23:16:17 -0500

    Fixed wrapping headers

--- wp-content/themes/roots-master/single.php
+++ wp-content/themes/roots-master/single.php
@@ -7,10 +7,5 @@
 <div class="shadow"></div>
 
 
+<?php get_template_part('templates/content', 'single'); ?>
 
-
-<div class="col-md-5 col-md-offset-6 col-sm-6 col-sm-offset-6 single-portfolio">
-	<div class="extra-pad">
-        <?php get_template_part('templates/content', 'single'); ?>
-    </div>
-</div>
