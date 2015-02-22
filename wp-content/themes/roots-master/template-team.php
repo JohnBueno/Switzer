5d17d5ad7eba9dbef7b28de08b60509cc6470ee8
John Bueno <bueno@facultycreative.com>
2015-02-22 09:44:08 -0500

    reorganized less

--- wp-content/themes/roots-master/template-team.php
+++ wp-content/themes/roots-master/template-team.php
@@ -15,16 +15,6 @@ Template Name: Team Template
 <div class="shadow"></div>
 	<?php while (have_posts()) : the_post(); ?>
     
-        <div class="col-md-5 col-xs-12 col-md-offset-6 col-sm-offset-6 col-sm-6">
-            <div class="extra-pad">
-                <div class="page-header">
-                    <div class="breadcrumbs">
-                        <?php the_breadcrumb(); ?>
-                    </div>
-                </div>
-            </div>
-        </div>
-    
                 
 	<?php get_template_part('templates/content', 'team'); ?>
   		
