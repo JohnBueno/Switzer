e4f2c070d78025e499878021a092781d83ffcf46
John Bueno <bueno@facultycreative.com>
2015-03-02 22:13:08 -0500

    made names go under header

--- wp-content/themes/roots-master/page.php
+++ wp-content/themes/roots-master/page.php
@@ -5,25 +5,22 @@
     ?>
     <li><img src="<?php echo($slide['slide']['url']); ?>"/></li>
     <?php endforeach; ?>
-    
 </ul>
 <div class="shadow"></div>
 
-    	<?php while (have_posts()) : the_post(); ?>
-            <div class="col-md-5 col-md-offset-6 col-sm-offset-6 col-sm-6 page white-bg">
-                <div class="extra-pad">
-          		    <?php get_template_part('templates/page', 'header'); ?>
-                </div>
-            </div>
-            <div class="row mobile-img">
-                <img  class="img-responsive" src="<?php echo($slides[0]['slide']['sizes']['large']); ?>" alt="<?php the_title(); ?>">
-            </div>
-            
-            <div class="col-md-5 col-md-offset-6 col-sm-offset-6 col-sm-6 page white-bg">
-                <div class="extra-pad">
-          		    <?php get_template_part('templates/content', 'page'); ?>
-                </div>
-            </div>
-    	<?php endwhile; ?>
+<?php while (have_posts()) : the_post(); ?>
+    <div class="col-md-5 col-md-offset-6 col-sm-offset-6 col-sm-6 page white-bg">
+        <div class="extra-pad">
+  		    <?php get_template_part('templates/page', 'header'); ?>
+        </div>
     </div>
-</div>
+    <div class="row mobile-img">
+        <img  class="img-responsive" src="<?php echo($slides[0]['slide']['sizes']['large']); ?>" alt="<?php the_title(); ?>">
+    </div>
+    
+    <div class="col-md-5 col-md-offset-6 col-sm-offset-6 col-sm-6 page white-bg">
+        <div class="extra-pad">
+  		    <?php get_template_part('templates/content', 'page'); ?>
+        </div>
+    </div>
+<?php endwhile; ?>
