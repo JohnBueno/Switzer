dabf7ab3eb03a928702a0c2db3e8070660b9d5fc
John Bueno <bueno@facultycreative.com>
2015-03-02 22:00:57 -0500

    set up basic style for portfolio page

--- wp-content/themes/roots-master/single.php
+++ wp-content/themes/roots-master/single.php
@@ -1,9 +1,4 @@
-<ul id="cbp-bislideshow" class="cbp-bislideshow">
-    <?php if($bg = get_field('background')): ?>
-        <li><img src="<?php echo($bg['url']); ?>"/></li>
-    <?php endif; ?>
-    
-</ul>
+
 <div class="shadow"></div>
 
 
--- wp-content/themes/roots-master/templates/content-single.php
+++ wp-content/themes/roots-master/templates/content-single.php
@@ -1,98 +1,39 @@
-
 <?php while (have_posts()) : the_post(); ?>
-<div class="col-md-5 col-md-offset-6 col-sm-6 col-sm-offset-6 single-portfolio">
-    <div id="trigger-open" class="extra-pad">
-        <article <?php post_class(); ?>>
-            <div class="page-header">
-                <div class="breadcrumbs">
-                    <ul id="breadcrumbs">
-                        <li>
-                        <a href="<?php bloginfo('url'); ?>/portfolio">Portfolio</a>
-                            
-                        </li>
-                    </ul>
+<?php
+    $slides = get_field('slides');
+?>
+
+<div class="header-fixed">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-6 thumbs">
+                <?php foreach ($slides as $slide): ?>
+                <div class="col-xs-1">
+                    <a href="#">
+                        <img class="img-responsive" src="<?php echo($slide['slide']['sizes']['thumbnail']); ?>"/>
+                    </a>
                 </div>
-            </div>
-            <header>
-                <div class="row">
-                    <div class="col-xs-8">
-                        <h3><?php the_title(); ?></h3>
+                <?php endforeach; ?>
+            </div> 
+            <div class="col-md-6 col-sm-6">
+                <div class="page-header">
+                    <div class="breadcrumbs">
+                        <ul id="breadcrumbs">
+                            <li>PORTFOLIO</li>
+                        </ul>
                     </div>
-                    <div class="col-xs-4">
-                        <h3 class="expand-portfolio">
-                            <a id="expand-portfolio" href="#">
-                                <small class="pull-right">Expand &nbsp;&nbsp;<i class="fa fa-plus"></i></small>
-                            </a>
-                        </h3>
-                    </div>
-                </div>
-            </header>
-            <div class="entry-content">
-                <div class="dropdown">
-                <hr class="visible-xs">
-                <?php the_content(); ?>
-                <div class="row">
-                    <div class="col-xs-6">
-                        <h5>Location</h5>
-                        <p><?php the_field('location');?></p>
-                    </div>
-                    <div class="col-xs-6">
-                        <h5>Size</h5>
-                        <p><?php the_field('size');?></p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-xs-6">
-                        <h5>Project Type</h5>
-                        <?php the_category('<br/>'); ?>
-                    </div>
-                    <div class="col-xs-6">
-                        <h5>Status</h5>
-                        <p><?php the_field('status');?></p>
-                    </div>
-                </div>
-                <div class="row links">
-                    <div class="col-xs-6">
-                        <p>  
-                            <?php
-                                $i = 0; 
-                                $slides = get_field('slides');
-                                foreach ($slides as $slide):
-                            ?>
-                                <?php if ($i==0): ?>
-                                    <a class="boxer" data-gallery="gallery1" href="<?php echo($slide['slide']['url']); ?>" title="<?php the_title(); ?>, <?php the_field('location');?>">
-                                        <strong><i class="fa fa-caret-right"></i> <small>View Full Gallery</small></strong>
-                                    </a>
-                               
-                                <?php else: ?>
-                                     <a href="<?php echo($slide['slide']['url']); ?>" title="<?php the_title(); ?>, <?php the_field('location');?>" class="boxer hide" data-gallery="gallery1"></a>
-                                <?php endif; ?>    
-                            <?php 
-                                $i++;
-                                endforeach; 
-                            ?>
-                            
-                            <br/>
-                            <?php 
-                                $cats = get_the_category();
-                                $catId = $cats[0]->cat_ID;
-                            ?>
-                        </p>
-                    </div>
-                    <div class="col-sm-6">
-                        <p>
-                            <a href="<?php bloginfo('url'); ?>/portfolio">
-                                <strong><i class="fa fa-caret-right"></i> <small>Back To Portfolio</small></strong>
-                            </a>
-                        </p>
-                    </div>
-                </div>
-                </div>
-            </div>
-        </article>
+                    <h3><?php the_title(); ?></h3>
+                </div>  
+            </div> 
+        </div>
     </div>
 </div>
-<a href="<?php echo($slides[0]['slide']['sizes']['large']); ?>" data-gallery="gallery2" class="background-boxer boxer hidden-xs"></a>
+
+<ul id="cbp-bislideshow" class="cbp-bislideshow">
+    <?php foreach ($slides as $slide): ?>
+        <li><img src="<?php echo($slide['slide']['url']); ?>"/></li>
+    <?php endforeach; ?>
+</ul>
 
 <div class="mobile-portfolio">
     <div style="margin:0;" class="row mobile-img">
@@ -104,13 +45,15 @@
         $i = 1; 
         foreach ($slides as $slide):
     ?>
+        <!--
         <?php if ($i==1): ?>
             <a href="<?php echo($slide['slide']['url']); ?>" class="gallery-binext boxer" title="<?php the_title(); ?>, <?php the_field('location');?>" data-gallery="gallery2"></a>
         <?php elseif($i==count($slides)): ?>
             <a href="<?php echo($slide['slide']['url']); ?>" class="gallery-biprev boxer" title="<?php the_title(); ?>, <?php the_field('location');?>" data-gallery="gallery2"></a>
         <?php else: ?>
             <a href="<?php echo($slide['slide']['url']); ?>" class="hide boxer" title="<?php the_title(); ?>, <?php the_field('location');?>" data-gallery="gallery2"></a>
-        <?php endif; ?>    
+        <?php endif; ?>  
+        -->  
     <?php 
         $i++;
         endforeach; 
